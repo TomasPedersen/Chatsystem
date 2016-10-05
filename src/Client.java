72d960fe92b1c9e7d6cb1029ce952b17736fbbcb
patinandroid <patina@dbmail.dk>
2016-10-05 09:02:32 +0200

    It works

--- src/Client.java
+++ src/Client.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 
 /**
  * Created by tomas on 10/4/16.
+ * Connect to server.
  */
 public class Client {
-    static Socket clientSocket = null;
-    static PrintStream outputStream = null;
-    static Scanner inputStream = null;
+    private static Socket clientSocket = null;
+    private static PrintStream outputStream = null;
+    private static Scanner inputStream = null;
 
 
-    public static void main(String[] args) {
-        try {
+	public static void main(String[] args) {
+		try {
 
-            clientSocket = new Socket("localhost", 2222);
-            inputStream = new Scanner(clientSocket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(inputStream.nextLine());
-    }
+			clientSocket = new Socket("localhost", 2222);
+			inputStream = new Scanner(clientSocket.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(inputStream.nextLine());
+		//TODO Send message to server.
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -2,24 +2,27 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by tomas on 10/4/16.
+ * Accept connections from clients.
  */
 public class Server {
-    static ServerSocket serverSocket = null;
-    static Socket clientSocket = null;
-    static PrintStream outputStream = null;
+	private static ServerSocket serverSocket = null;
+	private static Socket clientSocket = null;
+	private static PrintStream outputStream = null;
+	private static Scanner inputStream = null;
 
-    public static void main(String[] args) {
-        try {
-            serverSocket = new ServerSocket(2222);
-            clientSocket = serverSocket.accept();
-            outputStream = new PrintStream(clientSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        outputStream.println("You are connecting from: " + clientSocket.getInetAddress());
-        System.out.println(clientSocket.getInetAddress());
-    }
+	public static void main(String[] args) {
+		try {
+			serverSocket = new ServerSocket(2222);
+			clientSocket = serverSocket.accept();
+			outputStream = new PrintStream(clientSocket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		outputStream.println("You are connecting from: " + clientSocket.getInetAddress());
+		System.out.println(clientSocket.getInetAddress());
+	}
 }
